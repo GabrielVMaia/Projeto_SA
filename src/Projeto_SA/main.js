117713a3c92d7f96dacc222a5510d7d9f69bb1a7
GabrielVMaia <recoveryannulateduck@gmail.com>
2025-07-12 10:21:47 -0300

    🛠️ Commit inicial

---
+++ src/Projeto_SA/main.js
@@ -0,0 +1,13 @@
+/*
+    PARA WILLIAM
+
+    TODO:
+        Interações básicas dos botões (Apenas o de modificar informações, com o icone de lapis)
+        Interação pra fechar o pop-up do icone do lapis
+
+    O elemento dentro do html é: (do pop-up)
+     <button id="write" class="write"><i class="fa-solid fa-pencil"></i></button>
+*/
+
+
+
